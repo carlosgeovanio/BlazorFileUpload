b09ac914cef121d8823900d0af758fcf50419b9d
Gil <carlosgeovaniosilva@gmail.com>
2023-05-17 10:00:10 -0300

    Add new form and fields in Razor

---
+++ BlazorFileUpload/Data/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFileUpload.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "Carlos";
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Blazor Serve App
-Best Practices for File Uploading with SQL Server (capturing, displaying files, etc..)
+Best Practices for File Uploading with Blazor and SQL Server (capturing, displaying, etc..)
