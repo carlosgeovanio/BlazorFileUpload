951dea53273cea731d58ebaa1cca20defab9f913
Gil <carlosgeovaniosilva@gmail.com>
2023-05-18 11:10:00 -0300

    Capturing Uploaded Files in SQL

---
+++ UploadFilesLibrary/SqlDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace UploadFilesLibrary;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public async Task SaveData(string storeProc, string connectionName, object parameters)
+    {
+        string connectionString = config.GetConnectionString(connectionName)
+            ?? throw new Exception($"Missing connection string at {connectionName}");
+
+        using var connection = new SqlConnection(connectionString);
+
+        await connection.ExecuteAsync(storeProc, parameters, commandType: System.Data.CommandType.StoredProcedure);
+    }
+}
+
